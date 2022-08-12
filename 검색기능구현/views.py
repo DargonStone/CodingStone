f0e6edb69de53b4c9b5125e138ec77e26084e651
CodingStone <kimyungsok1472@gmail.com>
2022-08-13 03:45:06 +0900

    Create views.py

---
+++ 검색기능구현/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
+
+def index(request):
+    Post_list = Post.objects.all()  # queryset containing all movies we just created
+    paginator = Paginator(Post_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request=request, template_name="blog/index.html", context={'Post_list': page_obj})
+
+
+# Create your views here.
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+
+    return render(
+        request,
+        'blog/post_detail.html',
+        {
+            'post': post
+        }
+    )
+
+def search(request):
+    blogs = Post.objects.all()
+    query = request.GET.get('q')
+    if query:
+        blogs = Post.objects.filter(
+            (Q(title__icontains=query) | Q(content__icontains=query))).distinct()
+        paginator = Paginator(blogs, 12)
+        page = request.GET.get('page')
+
+        try:
+            post_list = paginator.page(page)
+        except PageNotAnInteger:
+            Post_list = paginator.page(1)
+        except EmptyPage:
+            page(paginator.num_pages)
+
+        context = { 'Post_list' : Post_list}
+
+        return render(request, 'blog/search.html', context)
