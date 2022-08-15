42cd193f1ccfa85ffe137453531ca7d87d91a6d0
CodingStone <kimyungsok1472@gmail.com>
2022-08-15 20:23:50 +0900

    Update settings.py

--- Gunicorn 적용/settings.py
+++ Gunicorn 적용/settings.py
@@ -1,2 +1,12 @@
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static/')
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media/')
+
+SUMMERNOTE_CONFIG = {
+    'attachment_filesize_limit': 5 * 1024 * 1024
