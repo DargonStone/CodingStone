0ecacd967ffb73b8c1dd2b625e945d648dff61ff
CodingStone <kimyungsok1472@gmail.com>
2022-08-18 22:58:27 +0900

    Update urls.py

--- Gunicorn 적용/urls.py
+++ Gunicorn 적용/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
 
