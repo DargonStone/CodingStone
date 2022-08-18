2693bce0ce19a7000fcb376e5c00976f3ae2e04e
CodingStone <kimyungsok1472@gmail.com>
2022-08-18 22:58:51 +0900

    Update urls.py

--- 검색기능구현/urls.py
+++ 검색기능구현/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post', views.index),
+    path('', views.index),
     path('<int:post_id>', views.post_detail, name='post_detail'),
     path('search', views.search, name='search')
 ]
