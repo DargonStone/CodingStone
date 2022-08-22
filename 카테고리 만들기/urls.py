e3bccb9defd2118cbf3ecfec54599c856f9d3599
CodingStone <kimyungsok1472@gmail.com>
2022-08-23 05:23:41 +0900

    Update urls.py

--- 카테고리 만들기/urls.py
+++ 카테고리 만들기/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('<int:post_id>', views.post_detail, name='post_detail'),
-    path('search', views.search, name='search'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('search/', views.search, name='search'),
     path('category/<str:slug>/', views.category_page )
 ]
